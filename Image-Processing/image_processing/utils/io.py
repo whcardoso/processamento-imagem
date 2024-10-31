28600e072c99b96561bbe6e6c030be789b1bae8c
Wallace Henrique <109251057+whcardoso@users.noreply.github.com>
2024-10-31 18:31:06 -0300

    Add files via upload

---
+++ Image-Processing/image_processing/utils/io.py
@@ -0,0 +1,8 @@
+from skimage.io import inread, insave
+
+def read_image(path, is_gray = False):
+    image = inread(path, as_gray = is_gray)
+    return image
+
+def save_image(image, path):
+    insave(path, image)
